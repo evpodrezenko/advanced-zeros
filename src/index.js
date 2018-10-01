e39a8ba5251d033de00d1c34f1c729b0492c0336
evpodrezenko <evpodrezenko@gmail.com>
2018-10-01 23:14:08 +0300

    Calculating the number of zeros at the end of a number that is the factorial of a given number in a certain bese

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zerosCount = 0;
+
+  let primitive = 2;
+  let tempBase = base;
+  const primitivesPowerMap = new Map();
+
+  while (primitive <= tempBase) {
+    if (tempBase % primitive) {
+      primitive++;
+    } else {
+      let power = primitivesPowerMap.has(primitive) ? primitivesPowerMap.get(primitive) : 0;
+      primitivesPowerMap.set(primitive, ++power);
+
+      tempBase /= primitive;
+    }
+  }
+
+  for (let [prim, power] of primitivesPowerMap) {
+    const count = Math.floor(getZeros(number, prim) / power);
+    
+    if (zerosCount === 0 || zerosCount > count) {
+      zerosCount = count;
+    }
+  }
+
+  return zerosCount;
+}
+
+function getZeros(number, primitive) {
+  let count = 0;
+
+  while (number > 0) {
+    number = Math.floor(number / primitive);
+    count += number;
+  }
+
+  return count;
 }
